d073548aac6ef9b45110eef958c7b237e50adfe3
Andres Diaz <jrcasp11@gmail.com>
2025-03-11 22:52:00 -0600

    CSS fix

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -246,14 +246,14 @@ function ProductList() {
     }
     const styleObjUl = {
         display: 'flex',
-        justifyContent: 'space-between',
         alignItems: 'center',
-        width: '1100px',
+        maxWidth: '1100px',
     }
     const styleA = {
         color: 'white',
         fontSize: '30px',
         textDecoration: 'none',
+        
     }
     const alignH1 = { textAlign: 'center', }
     return (
@@ -273,7 +273,7 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" style={styleA}>Plants</a></div>
-                    <div> <a href="#" style={styleA} onClick={handleCart}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="16"></circle><circle cx="184" cy="216" r="16"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg>{totalItems > 0 && <span className="cart_quantity_count">{totalItems}</span>}
+                    <div> <a href="#" className="styleB" onClick={handleCart}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="16"></circle><circle cx="184" cy="216" r="16"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg>{totalItems > 0 && <span className="cart_quantity_count">{totalItems}</span>}
                     </h1></a></div>
                 </div>
             </div>
@@ -291,7 +291,7 @@ function ProductList() {
                                         <div className='product-title'>{plant.name}</div>
                                         <div className='product-description'>{plant.description}</div>
                                         <div className='product-cost'>{plant.cost}</div>
-                                        <button className='product-button' disabled={buttonDisabled(plant.name) ? 'disabled' : ''} onClick={() => handleAddToCart(plant)}>
+                                        <button className={`product-button ${buttonDisabled(plant.name) ? 'active' : ''}`} disabled={buttonDisabled(plant.name) ? 'disabled' : ''} onClick={() => handleAddToCart(plant)}>
                                             {buttonDisabled(plant.name) ? 'Added to cart' : 'Add to cart'}</button>
                                     </div>
                                 ))}
